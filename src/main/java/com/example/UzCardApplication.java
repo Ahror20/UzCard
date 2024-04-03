5fc07af07a17299d98d07f9c2c1682906e460c21
Ahror20 <151931051+Ahror20@users.noreply.github.com>
2024-04-03 14:09:21 +0500

    Init commit

---
+++ src/main/java/com/example/UzCardApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UzCardApplication.class, args);
+    }
+
+}
